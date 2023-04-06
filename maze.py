cad3b896002b759ec622fd0a51e6885766ca5b49
roschlynnSLU <127329528+roschlynnSLU@users.noreply.github.com>
2023-04-06 02:11:11 +0000

    Added notes and comments maze.py

--- maze.py
+++ maze.py
@@ -1,65 +1,69 @@
 import random
 
+#Graph class which defines the functions and structures of the graph
 class Graph:
-    def __init__(self, num_nodes):
-        self.num_nodes = num_nodes
-        self.graph = {}
-        
-    def add_edge(self, u, v):
-        if u in self.graph:
-            self.graph[u].append(v)
-        else:
-            self.graph[u] = [v]
+    def __init__(self, num_nodes): #Start initialization
+        self.num_nodes = num_nodes # Total number of Nodes
+        self.graph = {} # Initializing graph as a dictionary
     
-    def remove_edge(self, u, v):
-        if u in self.graph:
-            if v in self.graph[u]:
-                self.graph[u].remove(v)
+    #Dictionary is key value pair (key, value)
+        
+    def add_edge(self, u, v): # Adding edges (u: starting vertex, v: ending vertex)
+        if u in self.graph: #First check if u already exists in the dictionary or not
+            self.graph[u].append(v) #append the ending node to the key value pair of the graph start node u
+        else: #Next case
+            self.graph[u] = [v] #create a new key value pair of the starting node and ending node
+    
+    def remove_edge(self, u, v): #Remove edges between two nodes (u: starting vertex, v: ending vertex)
+        if u in self.graph: #First check if u is a part of the adjacency list or not
+            if v in self.graph[u]: # Then check if v is a part of the key value pair that is it is connected to u or not
+                self.graph[u].remove(v) #Remove the edge between u and v
 
-    def has_edge(self, u, v):
-        result = False
-        if u in self.graph:
-            if v in self.graph[u]:
-                result = True
-        return result
+    def has_edge(self, u, v): #Check whether edges exist between two nodes
+        result = False #By default assume no edge exists between two nodes
+        if u in self.graph: #First check if the starting node is present or not in the adjacency list
+            if v in self.graph[u]: #Then check if an edge exists between the starting node and the ending node
+                result = True #Set result as true if it exists
+        return result #return true or false
 
-    def get_spanning_tree(self, start):
-        spanning_tree = Graph(self.num_nodes)
-        stack = []
-        visited = [False]*self.num_nodes
+    def get_spanning_tree(self, start): # Get spanning tree of the graph from the start node
+        spanning_tree = Graph(self.num_nodes) #Get total number of nodes of the tree from the self.num_nodes function
+        stack = [] #Initialize an empty stack
+        visited = [False]*self.num_nodes #Set visited array like you would do in dfs
 
-        visited[start] = True
-        stack.append(start)
-        while stack:
-            node = stack[len(stack)-1]
-            unvisited_nodes = []
-            for next_node in self.graph[node]:
-                if not visited[next_node]:
-                    unvisited_nodes.append(next_node)
-            if unvisited_nodes:
-                next_node = random.choice(unvisited_nodes)
-                stack.append(next_node)
-                visited[next_node] = True
-                spanning_tree.add_edge(node, next_node)
-                spanning_tree.add_edge(next_node, node)
-            else:
-                stack.pop()
+        visited[start] = True #Set start node as visited true in the visited array
+        stack.append(start) #Append the start node to the stack
+        while stack: #Keep executing until the stack is empty
+            node = stack[len(stack)-1] #Pop the last element of the stack
+            unvisited_nodes = [] #Initialize unvisited array
+            for next_node in self.graph[node]: #Get all the unvisted nodes from the graph adjacency list from the last popped element of the stack
+                if not visited[next_node]: #Check if the node that was connected to the node popped from the stack was visited or not
+                    unvisited_nodes.append(next_node) #Append all the unvisited nodes to the unvisited nodes array
+            if unvisited_nodes: #If any elements exists in the unvisited array
+                next_node = random.choice(unvisited_nodes) #select a random node using random.choice function from the unvisited nodes array
+                stack.append(next_node) #append the randomly selected node to the stack
+                visited[next_node] = True #Set the randomly selected node as visited true in the visited array
+                spanning_tree.add_edge(node, next_node) #Add an edge between the popped node at the start of the function and the randomly selected node
+                spanning_tree.add_edge(next_node, node) #Add an edge between the randomly selected node and the popped node at the start of the function
+            else: #If no elements in the unvisited nodes
+                stack.pop() #Pop the stack
 
-        return spanning_tree
+        return spanning_tree #Return spanning tree to the function
 
 
 
 class Maze:
-    def __init__(self, size):
-        self.size = size
-        self.nodes = []
-        self.graph = Graph(size*size)
+    def __init__(self, size): #Initialize of the maze with the given size
+        self.size = size #Setting self.size to size
+        self.nodes = [] #Defining an empty nodes array
+        self.graph = Graph(size*size) #Define the size of the graph as a square instead of a rectangle
 
         # label the nodes from 0 to (N*N)-1
-        for i in range(0, self.size):
-            self.nodes.append([])
-            for j in range(0, self.size):
-                self.nodes[i].append(i*self.size + j)
+        #This function creates a 2D array of nodes for the graph
+        for i in range(0, self.size): #Start array loop from 0 to the size of the graph that was defined
+            self.nodes.append([]) #Initialize an empty list and append it to the nodes array to create a 2D array
+            for j in range(0, self.size): #Inner loop 
+                self.nodes[i].append(i*self.size + j) #Appends a new node to the inner loop of the array
 
         # each node in the graph is connected to UP, DOWN, LEFT, RIGHT (if they exist)
         for i in range(0, self.size):
